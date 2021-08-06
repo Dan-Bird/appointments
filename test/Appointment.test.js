567e4e568d3695cf2a92b5d2f68f96440eaab467
Daniel <info@daniel-bird.co.uk>
2021-08-06 12:36:51 +0100

    Refactor tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -3,22 +3,27 @@ import ReactDOM from 'react-dom';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
-  it('renders the customer first name', () => {
-    const customer = { firstname: 'Ashley' };
-    const container = document.createElement('div');
-    document.body.appendChild(container);
+  let container;
+  let customer;
+  const render = component => ReactDOM.render(component, container);
 
-    ReactDOM.render(<Appointment customer={customer} />, container);
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  it('renders the customer first name', () => {
+    customer = { firstname: 'Ashley' };
+
+    render(<Appointment customer={customer} />, container);
 
     expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
-    const customer = { firstname: 'Jordan' };
-    const container = document.createElement('div');
-    document.body.appendChild(container);
+    customer = { firstname: 'Jordan' };
 
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    render(<Appointment customer={customer} />, container);
 
     expect(container.textContent).toMatch('Jordan');
   });
