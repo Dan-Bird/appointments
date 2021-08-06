00bd59f01c5d0cb9c20b750115758e2bb4222d7d
Daniel <info@daniel-bird.co.uk>
2021-08-06 12:48:28 +0100

    Pass first AppointmentsDayView test

--- src/Appointment.jsx
+++ src/Appointment.jsx
@@ -3,3 +3,5 @@ import * as React from 'react';
 export const Appointment = ({ customer: { firstname } }) => (
   <div>{firstname}</div>
 );
+
+export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -9,7 +9,6 @@ describe('Appointment', () => {
 
   beforeEach(() => {
     container = document.createElement('div');
-    document.body.appendChild(container);
   });
 
   it('renders the customer first name', () => {
@@ -26,5 +25,20 @@ describe('Appointment', () => {
     render(<Appointment customer={customer} />, container);
 
     expect(container.textContent).toMatch('Jordan');
+  });
+});
+
+describe('AppointmentsDayView', () => {
+  let container;
+  const render = component => ReactDOM.render(component, container);
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  it('renders a div with the right id', () => {
+    render(<AppointmentsDayView appointments={[]} />, container);
+
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
   });
 });
